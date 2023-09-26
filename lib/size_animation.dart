d46f1f871847f7e70f4c93227f81f4c8b7a53d36
minwoo <minwoo@rowan.kr>
2023-09-26 14:28:23 +0900

    tap & size animation

---
+++ lib/size_animation.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/material.dart';
+
+class SizeAnimation extends StatelessWidget {
+  const SizeAnimation({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              child: Wrap(
+                alignment: WrapAlignment.spaceBetween,
+                direction: Axis.horizontal,
+                spacing: 4,
+                runSpacing: 12,
+                children: [
+                  CircleWidget(color: Colors.green),
+                  CircleWidget(color: Colors.red),
+                  CircleWidget(color: Colors.amber),
+                  CircleWidget(color: Colors.blue),
+                ],
+              ),
+            ),
+            Wrap(
+              alignment: WrapAlignment.spaceBetween,
+              direction: Axis.horizontal,
+              spacing: 4,
+              runSpacing: 12,
+              children: [
+                CircleWidget(color: Colors.orange),
+                CircleWidget(color: Colors.teal),
+                CircleWidget(color: Colors.yellow),
+              ],
+            ),
+            Wrap(
+              alignment: WrapAlignment.spaceBetween,
+              direction: Axis.horizontal,
+              spacing: 4,
+              runSpacing: 12,
+              children: [
+                CircleWidget(color: Colors.pinkAccent),
+                CircleWidget(color: Colors.teal),
+                CircleWidget(color: Colors.amber),
+                CircleWidget(color: Colors.yellow),
+              ],
+            ),
+            // Padding(
+            //   padding: EdgeInsets.only(left: 48),
+            //   child: Row(
+            //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            //     children: [
+            // CircleWidget(color: Colors.red),
+            // CircleWidget(color: Colors.amber),
+            // CircleWidget(color: Colors.blue),
+            // CircleWidget(color: Colors.green),
+            //     ],
+            //   ),
+            // ),
+            // Row(
+            //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            //   children: [
+            //     CircleWidget(color: Colors.red),
+            //     CircleWidget(color: Colors.amber),
+            //     CircleWidget(color: Colors.blue),
+            //     CircleWidget(color: Colors.green),
+            //   ],
+            // ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CircleWidget extends StatefulWidget {
+  final double padding;
+  final Color color;
+  const CircleWidget({required this.color, super.key, this.padding = 4});
+
+  @override
+  State<CircleWidget> createState() => _CircleWidgetState();
+}
+
+class _CircleWidgetState extends State<CircleWidget>
+    with TickerProviderStateMixin {
+  AnimationController? _controller;
+  Animation<double>? _animation;
+  double containerSize = 48;
+
+  void _animateContainer() {
+    _controller?.dispose();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 1200),
+      vsync: this,
+    );
+
+    _animation = CurvedAnimation(
+      parent: _controller!,
+      curve: Curves.linearToEaseOut, // 여기에 원하는 curve를 적용
+    );
+
+    _animation = Tween<double>(
+      begin: containerSize,
+      end: containerSize + 20,
+    ).animate(_animation!)
+      ..addListener(() {
+        setState(() {
+          containerSize = _animation!.value;
+        });
+      });
+    _controller!.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        _animateContainer();
+      },
+      child: Padding(
+        padding: EdgeInsets.all(widget.padding),
+        child: Container(
+          width: containerSize,
+          height: containerSize,
+          decoration: BoxDecoration(
+            color: widget.color,
+            shape: BoxShape.circle,
+          ),
+        ),
+      ),
+    );
+  }
+}
